55a1ddfc9fca453511160773e9948eb1ca8ce22f
Tyson Brost <tyson.brost@gmail.com>
2022-01-27 21:50:50 -0700

    commit

---
+++ Import.r
@@ -0,0 +1,2 @@
+IO_airfare <- read_excel("IO airfare.xls")
+colnames(IO_airfare) <- c("year", "origin", "destin", "id", "dist", "passen", "fare", "bkmtshr", "ldist", "y98", "y99", "y00", "lfare","ldistsq", "concen", "lpassen")
